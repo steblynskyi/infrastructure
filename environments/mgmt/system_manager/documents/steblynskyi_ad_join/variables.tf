d3c5154cd5620bf21105ac9fd09279e19fea4984
taras.steblynskyi <taras@kubexpert.com>
2024-11-02 18:16:06 -0400

    Adding documents to join AD

---
+++ environments/mgmt/system_manager/documents/steblynskyi_ad_join/variables.tf
@@ -0,0 +1,18 @@
+variable "environment" {
+  description = "Environment name"
+  default     = "Mgmt"
+}
+
+variable "region" {
+  description = "Which region envirment will be created"
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS Profile name on local machine for multi aws account use"
+  default     = "taras"
+}
+
+variable "tag_terraform" {
+  default = "True"
+}
